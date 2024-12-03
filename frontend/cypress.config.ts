b8e86393a41b29a8824ea1c19c578020067cc952
Gzd Akgn <gozzde0990@gmail.com>
2024-12-03 21:28:22 +0100

    "Added code coverage configuration with Istanbul. Gitignore Coverage files"

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "cypress";
+import codeCoverage from "@cypress/code-coverage/task";
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Set up the code coverage task
+      codeCoverage(on, config);
+      return config;
     },
   },
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import istanbul from "vite-plugin-istanbul";
 import dns from "dns";
 
 dns.setDefaultResultOrder("ipv4first");
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    istanbul({
+      include: "src/*",
+      exclude: ["node_modules", "test/"],
+      extension: [".js", ".jsx", ".ts", ".tsx"],
+      cypress: true,
+    }),
+  ],
 });
