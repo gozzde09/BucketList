b9cf37d667394c378d51864cf6e17daf01701da0
Gzd Akgn <gozzde0990@gmail.com>
2024-12-03 22:15:32 +0100

    "Added component tests "

--- frontend/cypress/e2e/spec.cy.ts
+++ frontend/cypress/e2e/spec.cy.ts
@@ -1,9 +1,10 @@
 describe("template spec", () => {
-  before(() => {
-    cy.exec("npm run dev");
-    cy.wait(5000);
-  });
-  it("passes", () => {
-    cy.visit("http://localhost:5173/");
-  });
+  // before(() => {
+  //   cy.exec("npm run dev");
+  //   cy.wait(5000);
+  // });
+  // it("passes", () => {
+  //   cy.visit("http://localhost:5173/");
+  // });
+  cy.contains("h2", "Bucket List").should("be.visible");
 });
