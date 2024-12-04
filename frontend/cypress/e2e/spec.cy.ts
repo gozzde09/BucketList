cdd3f2d03d288ab9af5a403b106d28aedacf2778
Gzd Akgn <gozzde0990@gmail.com>
2024-12-04 12:03:00 +0100

    testaarrr

--- frontend/cypress/e2e/spec.cy.ts
+++ frontend/cypress/e2e/spec.cy.ts
@@ -1,9 +1,10 @@
 describe("template spec", () => {
   it("passes", () => {
-    cy.visit("http://localhost:5173/");
+    cy.visit("http://127.0.0.1:5173/");
   });
 
   it("should render the title", () => {
+    cy.visit("http://127.0.0.1:5173/");
     cy.contains("h1", "BucketList").should("be.visible");
   });
 });
